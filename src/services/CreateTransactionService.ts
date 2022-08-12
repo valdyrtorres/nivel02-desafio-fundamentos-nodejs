8caf29fa37a0d9f3536ddb1e223236985771f9e2
Valdir Torres <valdyrtorres@yahoo.com.br>
2022-08-11 23:57:49 -0300

    :sparkles melhoria validando type balance e fim do desafio

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -16,9 +16,13 @@ class CreateTransactionService {
   }
 
   public execute({ title, value, type }: Request): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+      throw new Error('Transaction type is invalid');
+    }
+
     const { total } = this.transactionsRepository.getBalance();
 
-    if (type == 'outcome' && total < value) {
+    if (type === 'outcome' && total < value) {
       throw new Error('You do not have enough balance');
     }
 
